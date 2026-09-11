4fe2d86c87a470888938b079a3b811e9ec3a1414
Michael Casabuena <michaelcasabuena.dev@gmail.com>
2026-09-11 23:59:33 +0800

    added Sample2.java

--- java-iteration-examples/src/Sample2.java
+++ java-iteration-examples/src/Sample2.java
@@ -1,7 +1,7 @@
-public class Sample1 {
+public class Sample2 {
     public static void main(String[] args) {
 
-        for(int i = 1; i <=5; i++){ //outer loop
+        for(int i = 5; i >= 1; i--){ //outer loop
             // System.out.print(i);
             for(int j = 1; j <= i; j++){ //inner loop
                 System.out.print(" *");
